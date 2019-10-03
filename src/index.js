d50e497d996b4f4c74e27e9e2863daa7d4253460
Victory <raccooccon@gmail.com>
2019-10-03 15:43:20 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
+module.exports = function getTimeForEducation(focus = 'family', knowsProgramming = true, config = {family: 4}) {
+  let priority = arguments[2][arguments[0]];
+
+  return arguments[1] == true ? Math.ceil(800 / priority) : Math.ceil(1300 /priority); 
+};
   
